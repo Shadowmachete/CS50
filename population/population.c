83f1b4daacb704b8cd57b5c10500f7acdfbb3dda
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-07 10:16:23 +0000

    Sat, Oct 7, 2023, 6:16 PM +08:00

--- population/population.c
+++ population/population.c
@@ -5,14 +5,16 @@ int main(void)
 {
     // TODO: Prompt for start size
     int startSize = 0;
-    do {
+    do
+    {
         printf("Start size: ");
         scanf("%d", &startSize);
     }
     while (startSize < 9);
     // TODO: Prompt for end size
     int endSize = 0;
-    do {
+    do
+    {
         printf("End size: ");
         scanf("%d", &endSize);
     }
@@ -22,11 +24,11 @@ int main(void)
     int numBirths;
     int numDeaths;
     while (startSize < endSize) {
-        numBirths = (int)startSize/3;
-        numDeaths = (int)startSize/4;
+        numBirths = (int) startSize / 3;
+        numDeaths = (int) startSize / 4;
         startSize += numBirths;
         startSize -= numDeaths;
-        numYears ++;
+        numYears++;
     }
     // TODO: Print number of years
     printf("Years: %i\n", numYears);
