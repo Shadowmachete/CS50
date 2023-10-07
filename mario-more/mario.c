132031762c884ddf432e0d6e56585cb3a3c49a06
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-07 10:56:55 +0000

    Sat, Oct 7, 2023, 6:56 PM +08:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -5,8 +5,7 @@ int main(void)
 {
     int pyramidHeight = 0;
     while ((pyramidHeight < 1) || (pyramidHeight > 8)) {
-        printf("Input a height for the pyramid (between 1 and 8 inclusive): ");
-        scanf("%d", &pyramidHeight);
+        pyramidHeight = get_int("Input a height for the pyramid (between 1 and 8 inclusive): ");
     }
     for (int layer = 1; layer < pyramidHeight + 1; layer++) {
         // spaces until the start of the brick
