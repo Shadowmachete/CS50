296c14e927cb7e33fba8d4e7c8553d1bef79ff74
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-07 10:30:27 +0000

    Sat, Oct 7, 2023, 6:30 PM +08:00

--- hello/hello.c
+++ hello/hello.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    char name;
-    printf("What's your name? ");
-    scanf("%s", &name);
-    printf("Hello, %c\n", name);
+    string name = get_string("What's your name? ");
+    printf("Hello, %s\n", name);
 }
