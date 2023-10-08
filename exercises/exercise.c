6938ecbf29a2a914415902b8e08f80f56de04cb0
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-08 08:29:53 +0000

    Sun, Oct 8, 2023, 4:29 PM +08:00

---
+++ exercises/exercise.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <cs50.h>
+
+void main(void)
+{
+    string name = get_string("What's your name? ");
+    int age = get_int("What's your age? ");
+    long phoneNumber = get_long("What's your phone number? ");
+}
