3f0249f04f81ee354891985160791227523185c8
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-08 09:20:31 +0000

    Sun, Oct 8, 2023, 5:20 PM +08:00

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -4,18 +4,13 @@
 int main(void)
 {
     long number = get_long("Input your credit card number: ");
-    long checksum = 0;
+    int checksum = 0;
     for (int i = 1; i < 16; i++)
     {
-        if (i % 2 == 0)
-        {
-            checksum += 2 * number % 10;
-        }
-        else
-        {
-            checksum += number % 10;
-        }
-        checksum /= 10;
+
     }
-    printf("%li\n", checksum);
+    if (checksum == 0)
+    {
+        
+    }
 }
