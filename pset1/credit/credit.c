44ba72f9b1d954f20ccd04e30de9133de6630d57
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-08 09:07:55 +0000

    Sun, Oct 8, 2023, 5:07 PM +08:00

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -1,6 +1,5 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <math.h>
 
 int main(void)
 {
@@ -9,15 +8,6 @@ int main(void)
     printf("%li\n", number % 10);
     for (int i = 1; i < 16; i++)
     {
-        long higherPower = pow(10, i);
-        long lowerPower = pow(10, i-1);
-        if (i % 2 == 0) {
-            checksum += 2 * (number % higherPower - number % lowerPower);
-        }
-        else
-        {
-            checksum += number % higherPower - number % lowerPower;
-        }
-        printf("%i\n", checksum);
+
     }
 }
