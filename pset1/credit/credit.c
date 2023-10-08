21ffcedd6ac98615c4a34f38a719c021e59c911d
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-08 09:06:03 +0000

    Sun, Oct 8, 2023, 5:06 PM +08:00

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -1,14 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
     long number = get_long("Input your credit card number: ");
     int checksum = 0;
     printf("%li\n", number % 10);
-    for (int i = 0; i < 16; i++)
+    for (int i = 1; i < 16; i++)
     {
-        checksum += 2 * (number % 10**(i*2) - number % 10**(i*2-1));
-        printf("%i", checksum);
+        long higherPower = pow(10, i);
+        long lowerPower = pow(10, i-1);
+        if (i % 2 == 0) {
+            checksum += 2 * (number % higherPower - number % lowerPower);
+        }
+        else
+        {
+            checksum += number % higherPower - number % lowerPower;
+        }
+        printf("%i\n", checksum);
     }
 }
