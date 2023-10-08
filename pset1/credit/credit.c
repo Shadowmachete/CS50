32af1e8ae0113b3288d1e5594fe48f63430c170e
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-08 10:13:55 +0000

    Sun, Oct 8, 2023, 6:13 PM +08:00

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -5,8 +5,13 @@ int main(void)
 {
     long number = get_long("Input your credit card number: ");
     int checksum = 0;
+    int checkTwo;
     for (int i = 0; i < 16; i++)
     {
+        if (number < 1000)
+        {
+            checkTwo = number;
+        }
         // loop through the number and add
         if (i % 2 == 1)
         {
@@ -22,19 +27,13 @@ int main(void)
     if (checksum % 10 == 0)
     {
         // getting first or first 2 digits
-        long checkFirstTwo = number;
-        printf("%li, %li\n", checkFirstTwo, (checkFirstTwo / 10));
-        while (checkFirstTwo >= 100)
-        {
-            checkFirstTwo /= 10;
-        }
-        printf("%li, %li\n", checkFirstTwo, (checkFirstTwo / 10));
+        printf("%i", checkTwo);
         // checking if first 2 digits satisfy condition
-        if (((int) (checkFirstTwo / 10)) == 4)
+        if (((int) (checkTwo / 10)) == 4)
         {
             printf("VISA\n");
         }
-        else if (checkFirstTwo == 34 || checkFirstTwo == 37)
+        else if (checkTwo == 34 || checkTwo == 37)
         {
             printf("AMEX\n");
         }
