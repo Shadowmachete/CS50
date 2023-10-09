2ef396f73aa3a265a89dcd3e719138ea5f86b2dd
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-09 09:59:59 +0000

    Mon, Oct 9, 2023, 5:59 PM +08:00

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -1,8 +1,28 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int count_letters(string text);
+
 int main(void)
 {
     string text = get_string("Text: ");
-    
+    int numLetters = count_letters(text);
+    int grade;
+    if (grade < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (grade >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", grade);
+    }
+}
+
+int count_letters(string text)
+{
+
 }
