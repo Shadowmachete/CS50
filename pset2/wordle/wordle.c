552eee81fce0efe7483e74b7fff59832ebdeaed4
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-09 11:14:43 +0000

    Mon, Oct 9, 2023, 7:14 PM +08:00

--- pset2/wordle/wordle.c
+++ pset2/wordle/wordle.c
@@ -141,7 +141,7 @@ int check_word(string guess, int wordsize, int status[], string choice)
                 // if they're the same position in the word, score EXACT points (green) and break so you don't compare that letter further
                 // if it's in the word, but not the right spot, score CLOSE point (yellow)
         // keep track of the total score by adding each individual letter's score from above
-
+    
     return score;
 }
 
