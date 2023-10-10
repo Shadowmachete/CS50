48a1de50157dd18aba8ae270d3655402f2610c6a
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-10 07:37:29 +0000

    Tue, Oct 10, 2023, 3:37 PM +08:00

--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max number of candidates
 #define MAX 9
@@ -102,7 +103,7 @@ bool vote(int rank, string name, int ranks[])
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(candidates[i].name, name) == 0)
+        if (strcmp(candidates[i], name) == 0)
         {
             ranks[i] = name;
             return true;
