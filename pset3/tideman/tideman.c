f8380109a8af57988ed311180147f2d995e4fcdf
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-10 12:13:04 +0000

    Tue, Oct 10, 2023, 8:13 PM +08:00

--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -202,7 +202,10 @@ void print_winner(void)
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        
+        for (int j = 0; j < candidate_count; j++)
+        {
+            
+        }
     }
     return;
 }
