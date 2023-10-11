48272f69a35d28705549afd42fd2b8de8dc57203
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-11 04:47:10 +0000

    Wed, Oct 11, 2023, 12:47 PM +08:00

--- lab4/volume/volume.c
+++ lab4/volume/volume.c
@@ -36,8 +36,7 @@ int main(int argc, char *argv[])
     // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
     fread(&header, sizeof(header), 1, input);
-    printf("%s\n", header);
-    fwrite(&header, sizeof(output), 1, output);
+    fwrite(&header, sizeof(header), 1, output);
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
     while (fread(&buffer, sizeof(buffer), 1, input))
