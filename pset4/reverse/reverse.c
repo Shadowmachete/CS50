3e63dc486e0304d4a86b90642310a7ef474e80d7
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-11 07:57:30 +0000

    Wed, Oct 11, 2023, 3:57 PM +08:00

--- pset4/reverse/reverse.c
+++ pset4/reverse/reverse.c
@@ -57,9 +57,10 @@ int main(int argc, char *argv[])
     fwrite(&header, sizeof(header), 1, output);
     // Use get_block_size to calculate size of block
     // TODO #7
-    
+    block_size = get_block_size(header);
     // Write reversed audio to file
     // TODO #8
+    
 }
 
 int check_format(WAVHEADER header)
@@ -79,5 +80,5 @@ int check_format(WAVHEADER header)
 int get_block_size(WAVHEADER header)
 {
     // TODO #7
-    return 0;
+    return header.numChannels * header.bitsPerSample / 8;
 }
