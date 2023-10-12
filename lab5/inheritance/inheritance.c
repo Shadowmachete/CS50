0ca63aecd70e161a14bd9a6fe0537032cd623a14
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-12 02:52:33 +0000

    Thu, Oct 12, 2023, 10:52 AM +08:00

--- lab5/inheritance/inheritance.c
+++ lab5/inheritance/inheritance.c
@@ -75,11 +75,15 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
-
+    if (p == NULL)
+    {
+        return;
+    }
     // TODO: Free parents recursively
-
+    free_family(p->parents[0]);
+    free_family(p->parents[1]);
     // TODO: Free child
-
+    free(p);
 }
 
 // Print each family member and their alleles.
