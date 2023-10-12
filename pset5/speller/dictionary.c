acb04cf3e4aa7f79dc89fbe195f55ea626238d83
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-12 07:33:06 +0000

    Thu, Oct 12, 2023, 3:33 PM +08:00

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -71,6 +71,7 @@ bool load(const char *dictionary)
             }
             new_node->next = NULL;
             table[hashedInt] = new_node;
+            free(new_node);
         }
         else
         {
@@ -90,6 +91,8 @@ bool load(const char *dictionary)
             }
             new_node->next = NULL;
             ptr->next = new_node;
+            free(ptr);
+            free(new_node);
         }
     }
     return true;
