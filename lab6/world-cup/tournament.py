59a1ba5d3fa73cdca7aea409e786e0c131daa13f
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-13 00:38:53 +0000

    Fri, Oct 13, 2023, 8:38 AM +08:00

--- lab6/world-cup/tournament.py
+++ lab6/world-cup/tournament.py
@@ -16,6 +16,7 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
+    with open(sys.argv[1])
     
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
