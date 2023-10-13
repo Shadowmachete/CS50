acd2844158767f92a74b43fbb4e2d393925701d2
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-13 02:23:29 +0000

    Fri, Oct 13, 2023, 10:23 AM +08:00

--- pset6/sentimental-credit/credit.py
+++ pset6/sentimental-credit/credit.py
@@ -11,9 +11,20 @@ number = str(number)
 
 checksum = 0
 for i in range(-1, -len(number)-1, -1):
+    print(checksum, number[i])
     if i % 2 == 0:
         checksum += 2 * int(number[i])
     else:
         checksum += int(number[i])
-
-if (checksum % 10 == 0) && (len(number) > )
+if (checksum % 10 == 0) and (len(number) >= 13) and (len(number) <= 18):
+    firstTwo = number[0:2]
+    if first[0] == '4':
+        print("VISA")
+    elif firstTwo == '34' or firstTwo == '37':
+        print("AMEX")
+    elif firstTwo == '51' or firstTwo == '52' or firstTwo == '53' or firstTwo == '54' or firstTwo == '55':
+        print("MASTERCARD")
+    else:
+        print("INVALID")
+else:
+    print("INVALID")
