516d42a2bb48a55498ea1bd14658fa5197e4e400
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-14 02:57:44 +0000

    Sat, Oct 14, 2023, 10:57 AM +08:00

---
+++ pset7/movies/12.sql
@@ -0,0 +1,1 @@
+SELECT title FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE title IN (SELECT title FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE name = "Bradley Cooper") AND name = "Jennifer Lawrence";
