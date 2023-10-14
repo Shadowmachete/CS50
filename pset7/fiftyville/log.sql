0bd2336368b2832ac6b1f054b48c8f03d0fc4ab4
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-14 04:03:10 +0000

    Sat, Oct 14, 2023, 12:03 PM +08:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -3,4 +3,4 @@ SELECT * FROM crime_scene_reports ;
 
 '| 295 | 2021 | 7     | 28  | Humphrey Street      | Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time - each of their interview transcripts mentions the bakery. |'
 
-SELECT * FROM interviews;
+SELECT * FROM interviews WHERE month = 7 AND day = 28 ;
