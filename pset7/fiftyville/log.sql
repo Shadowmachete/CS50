bc9a464a5142eab0cf7d56ab784efc8780220d5d
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-14 04:58:36 +0000

    Sat, Oct 14, 2023, 12:58 PM +08:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -8,7 +8,7 @@ SELECT * FROM crime_scene_reports WHERE month = 7 AND day = 28;
 -- Checking interviews with bakery mentioned in the transcript
 SELECT * FROM interviews WHERE month = 7 AND day = 28 AND transcript LIKE '%bakery%';
 
--- Deduction: Withdrew money from ATM on Leggett Street. Left the bakery parking lot within 10 minutes of the theft (10:15-10:25 am). Planned to take earlier flight out of Fiftyville on 29/7/2021
+-- Deduction: Withdrew money from ATM on Leggett Street. Left the bakery parking lot within 10 minutes of the theft (10:15-10:25 am). Planned to take earliest flight out of Fiftyville on 29/7/2021
 --| 161 | Ruth    | 2021 | 7     | 28  | Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away. If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame.                                                          |
 --| 162 | Eugene  | 2021 | 7     | 28  | I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.                                                                                                 |
 --| 163 | Raymond | 2021 | 7     | 28  | As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket. |
@@ -40,3 +40,5 @@ SELECT bakery_security_logs.id, name, people.license_plate FROM people JOIN bake
 -- | 267 | Kelsey  | 0NTHK55       |
 
 -- From combining the previous 2 we can see that suspect is either Bruce, Diana, Iman or Luca.
+
+-- Searching for earliest flights out of Fiftyville on 29/7/2021
