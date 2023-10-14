b2be429f72d643768253097b805d85e3ca6ae2fb
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-14 04:44:25 +0000

    Sat, Oct 14, 2023, 12:44 PM +08:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -2,10 +2,13 @@
 -- Checking for report about theft of duck
 SELECT * FROM crime_scene_reports WHERE month = 7 AND day = 28;
 
--- Important result: check interviews mentioning bakery!
+-- Deduction: check interviews mentioning bakery!
 --| 295 | 2021 | 7     | 28  | Humphrey Street      | Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time - each of their interview transcripts mentions the bakery. |
 
 -- Checking interviews with bakery mentioned in the transcript
 SELECT * FROM interviews WHERE month = 7 AND day = 28 AND transcript LIKE '%bakery%';
 
---
+-- Deduction: Withdrew money from ATM on Leggett Street. Left the bakery parking lot within 10 minutes of the theft
+--| 161 | Ruth    | 2021 | 7     | 28  | Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away. If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame.                                                          |
+--| 162 | Eugene  | 2021 | 7     | 28  | I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.                                                                                                 |
+--| 163 | Raymond | 2021 | 7     | 28  | As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket. |
