7f83a11ff58e0f10c0779061d162b1b58b641d35
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-14 03:04:42 +0000

    Sat, Oct 14, 2023, 11:04 AM +08:00

---
+++ pset7/movies/13.sql
@@ -0,0 +1,1 @@
+SELECT DISTINCT(name) FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE title IN (SELECT title FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE name = 'Kevin Bacon' AND birth = 1958) AND NOT name = 'Kevin Bacon';
