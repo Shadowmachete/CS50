7742ac3ad647de9a2fb8910c1cbe275ef71a4954
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-14 02:49:43 +0000

    Sat, Oct 14, 2023, 10:49 AM +08:00

---
+++ pset7/movies/11.sql
@@ -0,0 +1,1 @@
+SELECT title FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id JOIN ratings on movies.id = ratings.movie_id WHERE name = 'Chadwick Boseman' ORDER BY rating DESC LIMIT 5;
