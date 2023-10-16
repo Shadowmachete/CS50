966939bea42c5a3107ce4b93f64b8eb75c4f18b9
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-16 10:08:59 +0000

    Mon, Oct 16, 2023, 6:08 PM +08:00

--- lab9/birthdays/app.py
+++ lab9/birthdays/app.py
@@ -30,13 +30,15 @@ def index():
         name = request.form.get("name")
         month = request.form.get("month")
         day = request.form.get("day")
-        db.execute("INSERT INTO birthdays (name, month, day) VALUES (?)", name + "," + month + "," + day)
+        if not name or not month or not day:
+            return redirect("/")
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES (?, ?, ?)", name, month, day)
         return redirect("/")
 
     else:
 
         # TODO: Display the entries in the database on index.html
         birthdays = db.execute("SELECT * FROM birthdays")
-        return render_template("index.html")
+        return render_template("index.html", birthdays=birthdays)
 
 
