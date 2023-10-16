ef582429f1fadfa6daba6fbad8d514a94f461099
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-16 10:04:59 +0000

    Mon, Oct 16, 2023, 6:04 PM +08:00

--- lab9/birthdays/app.py
+++ lab9/birthdays/app.py
@@ -27,7 +27,10 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
-        db.execute("INSERT INTO birthdays (name, month, day) VALUES (?)", request.form.get())
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES (?)", name + "," + month + "," + day)
         return redirect("/")
 
     else:
