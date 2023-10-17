03533d36e0bb05710a5a3c5833c4607dd6c084ef
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-17 07:02:50 +0000

    Tue, Oct 17, 2023, 3:02 PM +08:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -37,8 +37,8 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    
-    return apology("TODO")
+
+    return render_template("index.html")
 
 
 @app.route("/buy", methods=["GET", "POST"])
