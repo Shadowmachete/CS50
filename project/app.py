084cda3e82eb897b89758f7df2a930fc0867916b
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-18 12:44:49 +0000

    Wed, Oct 18, 2023, 8:44 PM +08:00

--- project/app.py
+++ project/app.py
@@ -17,20 +17,36 @@ Session(app)
 def index():
     return render_template("index.html")
 
-@app.route("/layout")
-def layout():
-    return render_template("layout.html")
-
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    session.clear()
+
     if request.method == "POST":
-        ...
+        if not request.form.get("username"):
+            return
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return 
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
     else:
         return render_template("login.html")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        ...
+        return redirect("/login")
     else:
         return render_template("register.html")
