18bce458327b8a62b523b280dfc379968efb0778
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-18 07:39:23 +0000

    Wed, Oct 18, 2023, 3:39 PM +08:00

--- project/app.py
+++ project/app.py
@@ -1,10 +1,13 @@
-
+import sqlite3
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
+# Configure Application
 app = Flask(__name__)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+connection = sqlite3.connect('database.db')
