0410253f43e4088b2ef37a9bc91d101486e14709
Shadowmachete <127702841+Shadowmachete@users.noreply.github.com>
2023-10-21 03:00:20 +0000

    Sat, Oct 21, 2023, 11:00 AM +08:00

--- project/app.py
+++ project/app.py
@@ -8,6 +8,7 @@ from helpers import login_required
 # Configure Application
 app = Flask(__name__)
 
+
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
